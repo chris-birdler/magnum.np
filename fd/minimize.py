40e58598344107eb97f74ae47d84c661b3ea39a1
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-04-07 16:48:40 +0200

    make minimizer use pytorch

--- fd/minimize.py
+++ fd/minimize.py
@@ -1,42 +1,35 @@
-import numpy as np
-from scipy import integrate
+import torch
+from torchdiffeq import odeint
+import os
+CUDA_DEVICE = os.environ.get('CUDA_DEVICE', '0')
+cuda = torch.device(f"cuda:{CUDA_DEVICE}" if torch.cuda.is_available() else "cpu")
+
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                    level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
 class Minimizer(object):
     def __init__(self, terms):
         self._terms = terms
 
     def _dm(self, t, m):
-        h = np.sum([term.h(t, m) for term in self._terms], axis=0)
-        result =- np.cross(m, np.cross(m, h))
-        return result
+        h = sum([term.h(t, m) for term in self._terms])
+        return -torch.cross(m, torch.cross(m, h))
 
-    def minimize(self, m, rtol = 1e-4, dt = 1e-4, maxiter = np.inf):
+    def minimize(self, m, rtol = 1e-4, dt = 1e-4, mesh = None, method = 'dopri5', options = {'first_step':1e-7}):
         shape = m.shape
-        ode = integrate.ode(lambda t, m: self._dm(t, m.reshape(shape)).reshape(-1))
-        ode.set_initial_value(m.reshape(-1), 0.)
-        ode.set_integrator("dopri5")
+        m_old = torch.zeros_like(m)
+        t     = 0.
+        dmdt  = torch.inf
 
-        m_current = np.zeros(m.shape)
-        m_next    = m.copy()
-        t         = 0.
-        dmdt      = np.inf
-        i         = 0
+        counter = 0
+        while dmdt > rtol:
+            counter += 1
+            m_old[:,:,:,:] = m
 
-        with open('data/m0.log', 'w') as f:
-            while dmdt > rtol and i < maxiter:
-                t += dt
-                m_current[:] = m_next
-                m_next = ode.integrate(t).reshape(shape)
-                f.write("%g %g %g %g\n" % ((t,) + tuple(np.mean(m_next, axis=(0,1,2)))))
-                f.flush()
-                # TODO renormalize
-
-                # compute max norm of dm/dt
-                dmdt = np.linalg.norm((m_current - m_next).reshape(-1), ord = np.inf) / dt
-                # compute total energy
-                E = np.sum([term.E(t, m_next) for term in self._terms])
-
-                print("i=%d dmdt=%g E=%g" % (i, dmdt, E))
-                i += 1
-
-        return m_next
+            m = odeint(lambda t, m: self._dm(t, m), m, torch.DoubleTensor([t, t + dt], device=cuda), method=method, options=options)[1]
+            t += dt
+            dmdt = torch.linalg.norm((m - m_old).reshape(-1), ord = float("Inf")) / dt
+            E = sum([term.E(t, m) for term in self._terms])
+            logging.info("[MIN]: #%g, dmdt=%g E=%g" % (counter, dmdt.item(), E.item()))
+        return m
