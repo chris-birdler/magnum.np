8ea1547747e3bfd7bb73ef2018056daf04079a42
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-06-09 09:55:50 +0200

    fixed missing Ms in RKKY term

--- magnumnp/field_terms/rkky.py
+++ magnumnp/field_terms/rkky.py
@@ -4,6 +4,38 @@ import torch
 __all__ = ["RKKYField"]
 
 class RKKYField(object):
+    r"""
+    Effective field contribution corresponding to the RKKY interaction between two interface layers.
+
+    .. math::
+
+        E_\text{RKKY} = \int_\Gamma J_\text{RKKY} \, \vec{m}_1 \codt \vec{m}_2 d\vec{A}
+
+    :param state: current state
+    :type state: State
+
+    :Example:
+
+      .. code::
+
+        # create state with named domains from mesh
+        state = State(mesh)
+
+        # create domains as bool arrays, e.g:
+        domain1 = state.zeros(n, dtype=torch.bool)
+        domain1[n[0]//2:,:,:] = True
+
+        domain2 = state.zeros(n, dtype=torch.bool)
+        domain2[:-n[0]//2:,:,:] = True
+
+        # rotate magnetization within one subdomain
+        state.m[domain1] = torch.DoubleTensor([np.cos(phi), np.sin(phi), 0])
+
+        # without interface layer, two seperate exchange fields need to be defined
+        exchange1 = ExchangeField(state, Aex1, domain1)
+        exchange2 = ExchangeField(state, Aex2, domain2)
+        rkky = RKKYField(state, domain1, domain2)
+    """
     def __init__(self, state, J_rkky, dir, id1, id2):
         self._state = state
         self._mesh = state._mesh
@@ -20,33 +52,12 @@ class RKKYField(object):
     def h(self, t, m):
         self._h[:,:,(self._id2,),:] = self._J_rkky * m[:,:,(self._id1,),:]
         self._h[:,:,(self._id1,),:] = self._J_rkky * m[:,:,(self._id2,),:]
-        self._h *= 1./constants.mu_0/self._mesh.cell_volume
-        return self._h
+        self._h /= constants.mu_0 * self._Ms * self._mesh.cell_volume
+        return torch.nan_to_num(self._h)
 
     def E(self, t, m):
         E = (m[:,:,(self._id2,),:] * m[:,:,(self._id1,),:]).sum()
         E *= -self._mesh.dx[0]*self._mesh.dx[1]* self._J_rkky
         return E
 
-
-#### Interface
-#
-#state = State(mesh, cell_domains, facet_domains={"rkky1":1})
-#
-#domain1 = state.zeros(n, dtype=torch.bool)
-#domain1[n[0]//2:,:,:] = True
-#
-#domain2 = state.zeros(n, dtype=torch.bool)
-#domain2[:-n[0]//2:,:,:] = True
-#
-#state.m[domain1] = torch.DoubleTensor([np.cos(phi), np.sin(phi), 0])
-#
-#exchange1 = ExchangeField(state, Aex1, domain1)
-#exchange2 = ExchangeField(state, Aex2, domain2)
-#rkky = RKKYField(state, domain1, domain2)
-#
-### with interface layer
-#Aex["interface"] = 0
-#exchange = ExchangeField(state, Aex)
-#rkky = RKKYField(state, side1, side2)
 
