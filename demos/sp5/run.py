c9f844a26d9dd15bc072fb0c04c8f47ed1e9c3c6
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-06-28 13:39:44 +0200

    adapted demos

--- demos/sp5/run.py
+++ demos/sp5/run.py
@@ -1,6 +1,7 @@
-from fd import *
-import numpy as np
-from scipy import constants
+from magnumnp import *
+import torch
+
+Timer.enable()
 
 # initialize mesh
 n  = (40, 40, 1)
@@ -11,38 +12,42 @@ mesh = Mesh(n, dx)
 material = {
         "Ms": 8e5,
         "A": 1.3e-11,
-        "gamma": 2.211e5,
         "alpha": 0.1,
         "xi": 0.05,
         "b": 72.17e-12
         }
 
-# initialize field terms
-demag    = DemagField(mesh, material)
-exchange = ExchangeField(mesh, material)
-torque   = SpinTorque(mesh, material, [1e12, 0, 0])
+state = State(mesh, material)
 
 # initialize magnetization that relaxes into s-state
-m0 = np.zeros(n + (3,))
-m0[:20,:,:,1]   = -1.0
-m0[20:,:,:,1]   = 1.0
-m0[20,20,:,1]   = 0.
-m0[20,20,:,2]   = 1.
+state.m = state.zeros(n + (3,))
+state.m[:20,:,:,1] = -1.
+state.m[20:,:,:,1] = 1.
+state.m[20,20,:,1] = 0.
+state.m[20,20,:,2] = 1.
 
+state.j = torch.DoubleTensor((1e12, 0, 0))
+
+# initialize field terms
+demag    = DemagField(state)
+exchange = ExchangeField(state)
+torque   = SpinTorqueZhangLi(state)
 
 # initialize sstate
-minimizer = Minimizer([demag, exchange])
-m = minimizer.minimize(m0, 1e-2, 1e-4)
-#m = m0
+minimizer = Minimizer(state, [demag, exchange])
+minimizer.minimize_event()
 
 # perform integration with external field
-llg = LLG([demag, exchange, torque], material, m)
+llg = LLGSolver(state, [demag, exchange, torque])
 i = 0
-with open('sp5.dat', 'w') as f:
-    while llg.t < 10e-9:
+with open('data/m.dat', 'w') as f:
+    while state.t < 5e-9:
         if i % 10 == 0:
-            write_vtr(m, "sp5/m_%04d" % (i/10))
-        f.write("%g %g %g %g\n" % ((llg.t,) + tuple(np.mean(m, axis=(0,1,2)))))
+            write_vtr(state.m, "data/m_%04d" % (i/10))
+        f.write("%g %g %g %g\n" % ((state.t,) + tuple(torch.mean(state.m, axis=(0,1,2)))))
+        f.flush()
 
-        m = llg.step(1e-12)
+        m = llg.step(1e-10)
         i += 1
+
+Timer.print_report()
