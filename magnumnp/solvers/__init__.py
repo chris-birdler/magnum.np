0f241d9bc941bed3780368a978853ce75a6dbf48
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-08-05 15:59:24 +0200

    cleanup

--- magnumnp/solvers/__init__.py
+++ magnumnp/solvers/__init__.py
@@ -1,9 +1,7 @@
 from magnumnp.solvers.minimize import *
 from magnumnp.solvers.llg import *
 from magnumnp.solvers.rkf45 import *
-from magnumnp.solvers.adams45 import *
 
 __all__ = (minimize.__all__ +
            llg.__all__ +
-           rkf45.__all__ +
-           adams45.__all__)
+           rkf45.__all__)
