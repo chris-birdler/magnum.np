260c0c98b48bd64c14fcd95d280bccf6e1c92e7b
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-08-05 17:00:59 +0200

    fixed _arange

--- magnumnp/common/state.py
+++ magnumnp/common/state.py
@@ -50,9 +50,9 @@ class State(object):
         return x
 
     def SpatialCoordinates(self):
-        x = self.arange(self._mesh.n[0]) * self._mesh.dx[0] + self._mesh.dx[0]/2. + self._mesh.origin[0]
-        y = self.arange(self._mesh.n[1]) * self._mesh.dx[1] + self._mesh.dx[1]/2. + self._mesh.origin[1]
-        z = self.arange(self._mesh.n[2]) * self._mesh.dx[2] + self._mesh.dx[2]/2. + self._mesh.origin[2]
+        x = self._arange(self._mesh.n[0]) * self._mesh.dx[0] + self._mesh.dx[0]/2. + self._mesh.origin[0]
+        y = self._arange(self._mesh.n[1]) * self._mesh.dx[1] + self._mesh.dx[1]/2. + self._mesh.origin[1]
+        z = self._arange(self._mesh.n[2]) * self._mesh.dx[2] + self._mesh.dx[2]/2. + self._mesh.origin[2]
 
         XX, YY, ZZ = torch.meshgrid(x, y, z, indexing = "ij")
         return XX, YY, ZZ
