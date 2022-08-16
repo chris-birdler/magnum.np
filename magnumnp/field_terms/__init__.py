5a34a0df26963cd896d5d0dbece2a32f4cb74ae5
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-08-16 11:38:45 +0200

    removed dipole-only demag and oersted field

--- magnumnp/field_terms/__init__.py
+++ magnumnp/field_terms/__init__.py
@@ -1,25 +1,21 @@
 from magnumnp.field_terms.anisotropy import *
 from magnumnp.field_terms.demag import *
-from magnumnp.field_terms.demag_dipole import *
 from magnumnp.field_terms.demagPBC import *
 from magnumnp.field_terms.dmi import *
 from magnumnp.field_terms.exchange import *
 from magnumnp.field_terms.external import *
 from magnumnp.field_terms.oersted import *
-from magnumnp.field_terms.oersted_dipole import *
 from magnumnp.field_terms.rkky import *
 from magnumnp.field_terms.spintorque_zhangli import *
 from magnumnp.field_terms.spintorque_sot import *
 
 __all__ = (anisotropy.__all__ +
            demag.__all__ +
-           demag_dipole.__all__ +
            demagPBC.__all__ +
            dmi.__all__ +
            exchange.__all__ +
            external.__all__ +
            oersted.__all__ +
-           oersted_dipole.__all__ +
            rkky.__all__ +
            spintorque_zhangli.__all__ +
            spintorque_sot.__all__)
