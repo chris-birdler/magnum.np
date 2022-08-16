0ef1121f42dd5fe51ac5e88ac42e55588c07993a
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-08-16 10:30:03 +0200

    added D2dDMIField from Sabri

--- magnumnp/field_terms/dmi.py
+++ magnumnp/field_terms/dmi.py
@@ -1,7 +1,7 @@
 from magnumnp.common import timedmethod, constants
 import torch
 
-__all__ = ["InterfaceDMIField", "BulkDMIField"]
+__all__ = ["InterfaceDMIField", "BulkDMIField", "D2dDMIField"]
 
 class InterfaceDMIField(object):
     def __init__(self, state):
@@ -59,6 +59,29 @@ class BulkDMIField(object):
             dmydz = 0.
 
         h = -2. * self._Db/self._Js * torch.stack((dmydz-dmzdy, dmzdx-dmxdz, dmxdy-dmydx), dim=-1)
+        return torch.nan_to_num(h, posinf=0, neginf=0)
+
+    def E(self, t, m):
+        return -0.5 * constants.mu_0 * self._mesh.cell_volume * torch.sum(self._Js * m * self.h(t, m))
+
+
+class D2dDMIField(object):
+    def __init__(self, state):
+        self._state = state
+        self._mesh = state._mesh
+        self._Js = constants.mu_0 * self._state._material["Ms"]
+        self._DD2d = self._state._material["DD2d"]
+
+    @timedmethod
+    def h(self, t, m):
+        dim = [i for i in range(3) if self._mesh.n[i] > 1]
+        dx = [self._mesh.dx[i] for i in range(3) if self._mesh.n[i] > 1]
+
+        dmxdy = torch.gradient(self._state.m[:,:,:,0], spacing = dx[1], dim = 1)[0]
+        dmydx = torch.gradient(self._state.m[:,:,:,1], spacing = dx[0], dim = 0)[0]
+        dmzdx = torch.gradient(self._state.m[:,:,:,2], spacing = dx[0], dim = 0)[0]
+        dmzdy = torch.gradient(self._state.m[:,:,:,2], spacing = dx[1], dim = 1)[0]
+        h = -2.*self._DD2d/self._Js * torch.stack((dmzdy, dmzdx, -dmxdy-dmydx), dim=-1)
         return torch.nan_to_num(h, posinf=0, neginf=0)
 
     def E(self, t, m):
