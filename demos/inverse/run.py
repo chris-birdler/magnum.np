db6863ad66ffe3fab4d3b32527139c446901e80e
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-08-16 19:19:35 +0200

    fixed demo with parameters phi, theta via lambda function

--- demos/inverse/run.py
+++ demos/inverse/run.py
@@ -33,21 +33,21 @@ m_t[:, :, :, 0] = 1
 m_t[:, :, :, 2] = 1
 m_t.normalize()
 
-#phi = state.Tensor([0.], requires_grad = True)
-#theta = state.Tensor([1.5708], requires_grad = True)
-#h_ext = torch.stack([sin(theta)*cos(phi),
-#                     sin(theta)*sin(phi),
-#                     cos(theta)], dim=-1)
+phi = state.Tensor([0.], requires_grad = True)
+theta = state.Tensor([1.5708], requires_grad = True)
+h_ext = lambda t: torch.stack([sin(theta)*cos(phi),
+                               sin(theta)*sin(phi),
+                               cos(theta)], dim=-1)
 
-h_ext = state.Tensor([1,0,0], requires_grad = True)
+#h_ext = state.Tensor([1,0,0], requires_grad = True)
 
 demag    = DemagField(state)
 exchange = ExchangeField(state)
 aniso    = AnisotropyField(state)
 external = ExternalField(state, h_ext)
 
-#optimizer = torch.optim.Adam([phi, theta], lr=0.05)
-optimizer = torch.optim.Adam([h_ext], lr=0.05)
+optimizer = torch.optim.Adam([phi, theta], lr=0.05)
+#optimizer = torch.optim.Adam([h_ext], lr=0.05)
 my_loss = torch.nn.L1Loss()
 
 for epoch in range(100):
@@ -65,4 +65,5 @@ for epoch in range(100):
         L.backward()
     with Timer("optimizer"):
         optimizer.step()
-    print("h_ext:", h_ext)
+    print("phi:", phi)
+    print("theta:", theta)
