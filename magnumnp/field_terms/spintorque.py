c1b6481f8020bdc8e59515f392089eafb6ad9ff3
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-08-22 15:33:12 +0200

    make spintorque terms use state instead of t,m

--- magnumnp/field_terms/spintorque.py
+++ magnumnp/field_terms/spintorque.py
@@ -4,57 +4,27 @@ import torch
 __all__ = ["SpinOrbitTorque", "SpinTorqueZhangLi"]
 
 class SpinOrbitTorque(object):
-    def __init__(self, state):
-        self._state = state
-        self._mesh = self._state._mesh
-        self.eta_damp = self._state._material["eta_damp"]
-        self.eta_field = self._state._material["eta_field"]
-        self.Ms = self._state._material["Ms"]
-        self.je = self._state._material["je"]
-        self.d = self._state._material["d"] #thickness of FM
-        self._p = state.Tensor(state._material["p"])
-         
     @timedmethod
-    def h(self, t, m):
-        p = self._p(t)
-        h = self.eta_damp * torch.cross(m, p) + self.eta_field * p
-        h *= -self.je *constants.hbar / (2. * constants.e * self.Ms * constants.mu_0 * self.d) 
+    def h(self, state):
+        p = state.material["p"]
+        h = state.material["eta_damp"] * torch.cross(m, p) + state.material["eta_field"] * p
+        h *= -state.material["je"] * constants.hbar / (2. * constants.e * state.material["Ms"] * constants.mu_0 * state.material["d"])
         return torch.nan_to_num(h, posinf=0, neginf=0)
 
-    def E(self, t, m):
+    def E(self, state):
         raise NotImplemented()
 
 
 class SpinTorqueZhangLi(object):
-    def __init__(self, state):
-        self._state = state
-        self._mesh = state._mesh
-        self._b = self._state._material["b"]
-        self._xi = self._state._material["xi"]
-        if isinstance(self._state.j, list):
-            self._j = state.tensor(self._state.j)
-        elif isinstance(self._state.j, torch.Tensor):
-            if len(self._state.j.shape) == 4:
-                self._j = self._state.j[:,:,:,None,:]
-            else:
-                self._j = self._state.j
-        elif callable(self._state.j):
-            self._lambda_j = self._state.j
-        else:
-            raise TypeError("state.j needs to be 'list', 'torch.Tensor', or 'function'!")
-
     @timedmethod
-    def h(self, t, m):
-        if hasattr(self, "_lambda_j"):
-            j = self._state.tensor(self._lambda_j(t))
-        else:
-            j = self._j
+    def h(self, state):
+        dim = [i for i in range(3) if state.mesh.n[i] > 1]
+        dx = [state.mesh.dx[i] for i in range(3) if state.mesh.n[i] > 1]
 
-        dim = [i for i in range(3) if self._mesh.n[i] > 1]
-        dx = [self._mesh.dx[i] for i in range(3) if self._mesh.n[i] > 1]
-        jgradm = (torch.stack(torch.gradient(m, spacing=dx, dim=dim), dim=-1)*j[...,dim]).sum(axis=-1)
+        j = state.j(state.t)
+        jgradm = torch.einsum('...a,...ab-> ...b', j[...,dim], torch.stack(torch.gradient(state.m, spacing=dx, dim=dim), dim=-1)) # matmult
 
-        return self._b / constants.gamma * (torch.cross(m, jgradm) + self._xi * jgradm)
+        return state.material["b"] / constants.gamma * (torch.cross(state.m, jgradm) + state.material["xi"] * jgradm)
 
-    def E(self, t, m):
+    def E(self, state):
         raise NotImplemented()
--- tests/unit/spintorque_test.py
+++ tests/unit/spintorque_test.py
@@ -7,25 +7,24 @@ def test_zhangli():
     n  = (2,2,2)
     dx = (1e-9, 1e-9, 1e-9)
     mesh = Mesh(n, dx)
-    material = {
-            "Ms": 8e5,
-            "A": 1.3e-11,
-            "alpha": 0.1,
-            "xi": 0.05,
-            "b": 72.17e-12
-            }
+    state = State(mesh)
+    state.material = {
+        "Ms": 8e5,
+        "A": 1.3e-11,
+        "alpha": 0.1,
+        "xi": 0.05,
+        "b": 72.17e-12
+        }
     
-    state = State(mesh, material)
     state.m = state.Constant([0,1,0])
     state.m[:1,:,:,1] = -1.
     
-    state.j = state.tensor((1e12, 0, 0))
-    torque = SpinTorqueZhangLi(state)
-    h1 = torque.h(0, state.m)
+    state.j = state.Tensor((1e12, 0, 0))
+    torque = SpinTorqueZhangLi()
+    h1 = torque.h(state)
 
     state.j = state.Constant([1e12,0,0])
-    torque = SpinTorqueZhangLi(state)
-    h2 = torque.h(0, state.m)
+    h2 = torque.h(state)
 
     assert torch.allclose(h1, h2)
 
