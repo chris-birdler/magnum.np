51ed9b06967cc4c9e275f7f4bb07ecd24120eb03
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-09-20 09:39:10 +0200

    added utility functions

--- magnumnp/utils/__init__.py
+++ magnumnp/utils/__init__.py
@@ -1,3 +1,5 @@
 from magnumnp.utils.imaging_tools import *
+from magnumnp.utils.misc import *
 
-__all__ = (limaging_tool.__all__)
+__all__ = (imaging_tool.__all__ +
+           misc.__all__)
---
+++ magnumnp/utils/misc.py
@@ -0,0 +1,16 @@
+import torch
+
+__all__ = ["add_noise", "nsk"]
+
+def add_noise(x, dev = 1.0, mean = 0.0):
+   if torch.is_tensor(x):
+        x += torch.empty_like(x).normal_(mean = mean, std = dev) 
+        x.normalize()
+
+def nsk(x, axis = 2):
+    m = self.mean(axis=axis)
+    dxm = torch.stack(torch.gradient(m, spacing = dx[0], dim = 0), dim = -1).squeeze(-1)
+    dym = torch.stack(torch.gradient(m, spacing = dx[1], dim = 1), dim = -1).squeeze(-1)
+    nsk = 1./(4. * pi) * (m * torch.cross(dxm, dym)).sum()* dx[0] * dx[1]
+    return float(nsk.detach().cpu().numpy())
+
