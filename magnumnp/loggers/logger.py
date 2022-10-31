5cb361c9443ac05e497d3a81eb0639b9e327bf4a
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-10-31 16:33:17 +0100

    move resumed tensor to device

--- magnumnp/loggers/logger.py
+++ magnumnp/loggers/logger.py
@@ -72,7 +72,8 @@ class Logger(object):
         resumable_step = min(map(lambda logger: logger.resumable_step(), self.loggers.values()))
         assert resumable_step >= last_recorded_step + 1
 
-        state.m, state.t = self.loggers["fields"].step_data(last_recorded_step, "m")
+        m, state.t = self.loggers["fields"].step_data(last_recorded_step, "m")
+        state.m = m.to(device = state._device)
         logging.info_green("Resuming from step %d (t = %g)." % (last_recorded_step, state.t))
 
         for logger in self.loggers.values():
