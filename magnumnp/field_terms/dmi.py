501a5e8c141170b2203285b19017a1fc43418896
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-11-15 13:01:20 +0100

    added 1D interface DMI unit test

--- magnumnp/field_terms/dmi.py
+++ magnumnp/field_terms/dmi.py
@@ -50,51 +50,17 @@ class InterfaceDMIField(DMIField):
         super().__init__(dmi_vector, Di)
 
 
-class BulkDMIField(object):
-    @timedmethod
-    def h(self, state):
-        dx = state.mesh.dx
-
-        if state.mesh.n[0] > 1:
-            dmydx = torch.gradient(state.m[:,:,:,1], spacing = dx[0], dim = 0)[0]
-            dmzdx = torch.gradient(state.m[:,:,:,2], spacing = dx[0], dim = 0)[0]
-        else:
-            dmydx = 0.
-            dmzdx = 0.
-
-        if state.mesh.n[1] > 1:
-            dmxdy = torch.gradient(state.m[:,:,:,0], spacing = dx[1], dim = 1)[0]
-            dmzdy = torch.gradient(state.m[:,:,:,2], spacing = dx[1], dim = 1)[0]
-        else:
-            dmxdy = 0.
-            dmzdy = 0.
-
-        if state.mesh.n[2] > 1:
-            dmxdz = torch.gradient(state.m[:,:,:,0], spacing = dx[2], dim = 2)[0]
-            dmydz = torch.gradient(state.m[:,:,:,1], spacing = dx[2], dim = 2)[0]
-        else:
-            dmxdz = 0.
-            dmydz = 0.
-
-        h = -2. * state.material["Db"] / constants.mu_0 / state.material["Ms"] * torch.stack((dmydz-dmzdy, dmzdx-dmxdz, dmxdy-dmydx), dim=-1)
-        return torch.nan_to_num(h, posinf=0, neginf=0)
-
-    def E(self, state):
-        return -0.5 * constants.mu_0 * state.mesh.cell_volume * torch.sum(state.material["Ms"] * m * self.h(state))
+class BulkDMIField(DMIField):
+    def __init__(self, Db = "Db"):
+        dmi_vector = [[1, 0, 0], # x
+                      [0, 1, 0], # y
+                      [0, 0, 1]] # z
+        super().__init__(dmi_vector, Db)
 
 
-class D2dDMIField(object):
-    @timedmethod
-    def h(self, state):
-        dim = [i for i in range(3) if state.mesh.n[i] > 1]
-        dx = [state.mesh.dx[i] for i in range(3) if state.mesh.n[i] > 1]
-
-        dmxdy = torch.gradient(state.m[:,:,:,0], spacing = dx[1], dim = 1)[0]
-        dmydx = torch.gradient(state.m[:,:,:,1], spacing = dx[0], dim = 0)[0]
-        dmzdx = torch.gradient(state.m[:,:,:,2], spacing = dx[0], dim = 0)[0]
-        dmzdy = torch.gradient(state.m[:,:,:,2], spacing = dx[1], dim = 1)[0]
-        h = -2. * state.material["DD2d"] / constants.mu_0 / state.material["Ms"] * torch.stack((dmzdy, dmzdx, -dmxdy-dmydx), dim=-1)
-        return torch.nan_to_num(h, posinf=0, neginf=0)
-
-    def E(self, state):
-        return -0.5 * constants.mu_0 * state.mesh.cell_volume * torch.sum(state.material["Ms"] * m * self.h(state))
+class D2dDMIField(DMIField):
+    def __init__(self, DD2d = "DD2d"):
+        dmi_vector = [[-1, 0, 0], # x
+                      [ 0, 1, 0], # y
+                      [ 0, 0, 0]] # z
+        super().__init__(dmi_vector, DD2d)
--- tests/unit/dmi_test.py
+++ tests/unit/dmi_test.py
@@ -2,15 +2,15 @@ import pytest
 import pathlib
 import torch
 from magnumnp import *
+import numpy as np
 
 def test_call():
-    n  = (8,10,12)
+    n = (8,10,12)
     dx = (1e-9, 2e-9, 5e-9)
-    Ms = 1./constants.mu_0
     mesh = Mesh(n, dx)
     state = State(mesh)
     state.m = state.Constant([1,0,0])
-    state.material = {"Ms": Ms,
+    state.material = {"Ms": 1./constants.mu_0,
                       "Di": 1.,
                       "Db": 1.,
                       "DD2d": 1.}
@@ -22,25 +22,33 @@ def test_call():
     dmi = D2dDMIField()
     dmi.h(state)
 
-def test_exchange_only():
-    n  = (20, 20, 1)
-    dx = (0.5e-9, 0.5e-9, 1e-9)
-    mesh = Mesh(n, dx, origin = (-n[0]*dx[0]/2., -n[1]*dx[1]/2., 0.0))
+def test_interface_1D():
+    """ 1D Interface DMI Testcase
+        taken from 'Proposal for a micromagnetic standard problem for materials with Dzyaloshinskii–Moriya interaction'
+    """
+    n = (20, 1, 1) # (100, 1, 1) for discretization as in Paper
+    dx = (5e-9, 1e-9, 1e-9)
+    origin = (-n[0]*dx[0]/2., -n[1]*dx[1]/2., -n[2]*dx[2]/2.,)
+    mesh = Mesh(n, dx, origin)
     state = State(mesh)
-    state.material["Ms"] = state.Constant([1./constants.mu_0])
-    state.material["A"] = state.Constant([1e-12])
-    state.material["Di"] = state.Constant([0.])
-
+    state.material = {"Ms": 0.86e6,
+                      "A": 13e-12,
+                      "Ku": 0.4e6,
+                      "Ku_axis": [0,0,1],
+                      "Di": -3e-3,
+                      "alpha": 1.}
     x, y, z = state.SpatialCoordinates()
-    domain = x**2 + y**2 < 5e-9**2
+    state.m = state.Constant([0.1,0.0,1.0])
+    state.m.normalize()
 
-    state.m = torch.stack([-y,x,0*z], dim=-1)
-    state.m[~domain] = 0
+    exchange = ExchangeField()
+    aniso = UniaxialAnisotropyField()
+    dmi = InterfaceDMIField()
 
-    exchange1 = ExchangeField()
-    exchange2 = ExchangeDMIField()
-
-    h1 = exchange1.h(state)
-    h2 = exchange2.h(state)
-
-    torch.testing.assert_close(h1, h2)
+    llg = LLGSolver([exchange, aniso, dmi])
+    logger = ScalarLogger("data/m_relax.dat", ['t', 'm'])
+    while state.t <= 2e-10:
+        logger << state
+        llg.step(state, 1e-11)
+    assert state.m[...,0].max() == pytest.approx(0.443, rel=1e-2)
+    assert state.m[...,2].min() == pytest.approx(0.896, rel=1e-2)
