c7c96a92743a445490a3d28720766509f10003e2
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-11-16 23:32:05 +0100

    fixed operator [] overloading of (dim=1, shape=1) if element 0 is accessed

--- magnumnp/common/decorated_tensor.py
+++ magnumnp/common/decorated_tensor.py
@@ -39,7 +39,7 @@ class DecoratedTensor(torch.Tensor):
         return self
 
     def __getitem__(self, key):
-        if self.dim() == 0 or (self.dim() == 1 and self.shape[0] == 1):
+        if self.dim() == 0 or (self.dim() == 1 and self.shape[0] == 1 and key != 0):
             return self
         else:
             return super().__getitem__(key)
