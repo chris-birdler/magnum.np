a1edb6cb735d0ca225e410ecb4246fa7dfd778b1
Florian Bruckner <florian.bruckner@univie.ac.at>
2022-12-12 16:04:41 +0100

    make timeinterpolator return torch tensor

--- magnumnp/common/time_interpolator.py
+++ magnumnp/common/time_interpolator.py
@@ -3,19 +3,17 @@ from scipy import interpolate
 
 __all__ = ["TimeInterpolator"]
 
-class TimeInterpolator(object): # TODO: use torch in order to prevent additional type-cases
-    def __init__(self, points):
+class TimeInterpolator(object):
+    def __init__(self, state, points):
         xp = np.array(list(points.keys()))
         fp = np.array(list(points.values()))
         self._f = interpolate.interp1d(xp, fp, axis=0, fill_value="extrapolate")
         self._final_time = xp[-1]
+        self._state = state
 
     def __call__(self, t):
         result = self._f(t)
-        if result.shape == ():
-            return float(result)
-        else:
-            return result
+        return self._state.Tensor(result)
 
     @property
     def final_time(self):
