cabad3472794402279ac782c7b58b8a69bfb423f
Florian Bruckner <florian.bruckner@univie.ac.at>
2023-01-16 15:05:06 +0100

    also mock numpy library for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
 ]
-autodoc_mock_imports = ["torch"]
+autodoc_mock_imports = ["torch", "numpy"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
