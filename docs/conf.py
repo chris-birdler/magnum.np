323064c84643219ceb2accad7201afa11fe1c524
Florian Bruckner <florian.bruckner@univie.ac.at>
2023-01-16 14:26:12 +0100

    removed .readthedocs.yaml

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
 ]
+autodoc_mock_imports = ["torch"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
