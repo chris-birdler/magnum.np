d307155f9a31126dd448f5ed37149dd27218c82c
Florian Bruckner <florian.bruckner@univie.ac.at>
2023-03-22 08:11:20 +0100

    avoid padding in order to simplify code (need to check for n=1 or n=2)

--- magnumnp/solvers/ohm_solver.py
+++ magnumnp/solvers/ohm_solver.py
@@ -22,97 +22,58 @@ import torch
 __all__ = ["OhmSolver"]
 
 class OhmSolver(object):
-    def __init__(self, dirichlet_bc_nodes, dirichlet_bc_u0):
-        self._dirichlet_bc_u0 = dirichlet_bc_u0
+    def __init__(self, dirichlet_bc_nodes):
         self._dirichlet_bc_nodes = dirichlet_bc_nodes
 
     def u(self, state):
-        sigma = state.material["sigma"]
-        u0 = state._zeros(state.mesh.n)
-        u_pad = state._zeros([n+2 for n in state.mesh.n])
-        sigma_pad = state._zeros([n+2 for n in state.mesh.n])
-        sigma_pad[1:-1,1:-1,1:-1] = sigma[:,:,:,0]
+        sigma = state.material["sigma"].squeeze(-1)
         rhs = state._zeros(state.mesh.n)
+        u0 = state.u.clone()
 
         def _M(u):
-            lap = torch.zeros_like(u)
-
-            # setup padding magnetization
-            u_pad[1:-1,1:-1,1:-1] = u
-            #u_pad[0,1:-1,1:-1] = u[0,:,:,:] + g[0,:,:,:] * dx^? * sigma^?
+            res = torch.zeros_like(u)
 
             # homogeneous Neumann conditions for now
-            sigma_pad[ 0,1:-1,1:-1] = sigma[ 0,:,:,0]
-            sigma_pad[-1,1:-1,1:-1] = sigma[-1,:,:,0]
-            sigma_pad[1:-1, 0,1:-1] = sigma[:, 0,:,0]
-            sigma_pad[1:-1,-1,1:-1] = sigma[:,-1,:,0]
-            sigma_pad[1:-1,1:-1, 0] = sigma[:,:, 0,0]
-            sigma_pad[1:-1,1:-1,-1] = sigma[:,:,-1,0]
-
-            u_pad[ 0,1:-1,1:-1] = u[ 0,:,:]
-            u_pad[-1,1:-1,1:-1] = u[-1,:,:]
-            u_pad[1:-1, 0,1:-1] = u[:, 0,:]
-            u_pad[1:-1,-1,1:-1] = u[:,-1,:]
-            u_pad[1:-1,1:-1, 0] = u[:,:, 0]
-            u_pad[1:-1,1:-1,-1] = u[:,:,-1]
+            u[ 0,:,:] = u[ 1,:,:]
+            u[-1,:,:] = u[-2,:,:]
+            u[:, 0,:] = u[:, 1,:]
+            u[:,-1,:] = u[:,-2,:]
+            u[:,:, 0] = u[:,:, 1]
+            u[:,:,-1] = u[:,:,-2]
 
             # set Dirichlet conditions
-            u_pad[-1,1:-1,1:-1] = u[-1,:,:]
-            u_pad[self._dirichlet_bc_nodes] = self._dirichlet_bc_u0[self._dirichlet_bc_nodes]
+            u[self._dirichlet_bc_nodes] = u0[self._dirichlet_bc_nodes]
 
             # assemble laplace
-            sigma_avg = 2. * sigma_pad[1:,:,:] * sigma_pad[:-1,:,:] / (sigma_pad[1:,:,:] + sigma_pad[:-1,:,:])
+            # x
+            sigma_avg = 2. * sigma[1:,:,:] * sigma[:-1,:,:] / (sigma[1:,:,:] + sigma[:-1,:,:])
             sigma_avg = sigma_avg.nan_to_num(posinf=0, neginf=0)
-            du = u_pad[1:,:,:] - u_pad[:-1,:,:]
+            du = u[1:,:,:] - u[:-1,:,:]
             du *= sigma_avg / state.mesh.dx[0]
-            lap += (du[1:,1:-1,1:-1] - du[:-1,1:-1,1:-1]) / state.mesh.dx[0]
+            res[1:-1,:,:] += (du[1:,:,:] - du[:-1,:,:]) / state.mesh.dx[0]
 
-            sigma_avg = 2. * sigma_pad[:,1:,:] * sigma_pad[:,:-1,:] / (sigma_pad[:,1:,:] + sigma_pad[:,:-1,:])
+            # y
+            sigma_avg = 2. * sigma[:,1:,:] * sigma[:,:-1,:] / (sigma[:,1:,:] + sigma[:,:-1,:])
             sigma_avg = sigma_avg.nan_to_num(posinf=0, neginf=0)
-            du = u_pad[:,1:,:] - u_pad[:,:-1,:]
+            du = u[:,1:,:] - u[:,:-1,:]
             du *= sigma_avg / state.mesh.dx[1]
-            lap += (du[1:-1,1:,1:-1] - du[1:-1,:-1,1:-1]) / state.mesh.dx[1]
+            res[:,1:-1,:] += (du[:,1:,:] - du[:,:-1,:]) / state.mesh.dx[1]
 
-            sigma_avg = 2. * sigma_pad[:,:,1:] * sigma_pad[:,:,:-1] / (sigma_pad[:,:,1:] + sigma_pad[:,:,:-1])
+            # z
+            sigma_avg = 2. * sigma[:,:,1:] * sigma[:,:,:-1] / (sigma[:,:,1:] + sigma[:,:,:-1])
             sigma_avg = sigma_avg.nan_to_num(posinf=0, neginf=0)
-            du = u_pad[:,:,1:] - u_pad[:,:,:-1]
+            du = u[:,:,1:] - u[:,:,:-1]
             du *= sigma_avg / state.mesh.dx[2]
-            lap += (du[1:-1,1:-1,1:] - du[1:-1,1:-1,:-1]) / state.mesh.dx[2]
+            res[:,:,1:-1] += (du[:,:,1:] - du[:,:,:-1]) / state.mesh.dx[2]
 
-            return lap
+            return res
 
-        return conjugate_gradient(_M, u0, rhs)
+        state.u = conjugate_gradient(_M, state.u, rhs)
+        return state.u
 
-#    def j(self, state):
-#        sigma = state.material["sigma"] # TODO: add string parameter
-#        u = self.u(state)
-#
-#        # x
-#        du = u[1:,:,:] - u[:-1,:,:] # nx, ny+1, nz+1, 1]
-#        jx = du[:, 1:, 1:]
-#           + du[:, 1:,:-1]
-#           + du[:,:-1, 1:]
-#           + du[:,:-1,:-1]
-#        jx /= 4. * state.mesh.dx[0]
-#
-#        # y
-#        du = u[:,1:,:] - u[:,:-1,:] # nx+1, ny, nz+1, 1]
-#        jy = du[ 1:,:, 1:]
-#           + du[ 1:,:,:-1]
-#           + du[:-1,:, 1:]
-#           + du[:-1,:,:-1]
-#        jy /= 4. * state.mesh.dx[1]
-#
-#        # z
-#        du = u[:,:,1:] - u[:,:,:-1] # nx+1, ny+1, nz, 1]
-#        jz = du[ 1:, 1:,:]
-#           + du[ 1:,:-1,:]
-#           + du[:-1, 1:,:]
-#           + du[:-1,:-1,:]
-#        jz /= 4. * state.mesh.dx[2]
-#
-#        return -state.material.sigma * torch.stack([jx, jy, jz], dim = -1)
-
+    def j(self, state):
+        sigma = state.material["sigma"]
+        return -sigma * torch.stack(torch.gradient(self.u(state)), dim=-1)
 
 def conjugate_gradient(A, x, b, tol=1e-6, max_iter=1000):
     r = b - A(x)
--- tests/unit/ohm_solver_test.py
+++ tests/unit/ohm_solver_test.py
@@ -11,18 +11,17 @@ def test_cube():
     state = State(mesh)
     state.material = {"sigma": 1.}
 
-    dirichlet_bc_u0 = state._zeros([n+2 for n in state.mesh.n])
-    dirichlet_bc_nodes = state._zeros([n+2 for n in state.mesh.n], dtype=bool)
+    dirichlet_bc_nodes = state.Constant(False, dtype=bool)
     dirichlet_bc_nodes[0,:,:] = True
     dirichlet_bc_nodes[-1,:,:] = True
 
-    dirichlet_bc_u0[0,:,:] = 0.
-    dirichlet_bc_u0[-1,:,:] = 1.
+    state.u = state.Constant(0.)
+    state.u[-1,:,:] = 1.
 
-    print("test:", dirichlet_bc_nodes)
-    ohm = OhmSolver(dirichlet_bc_nodes, dirichlet_bc_u0)
+    ohm = OhmSolver(dirichlet_bc_nodes)
     u = ohm.u(state)
-    write_vti(u, "data/u.vti")
+    j = ohm.j(state)
+    write_vti({"u":u, "j":j}, "data/u.vti")
 
 def test_curved():
     n  = (120, 60, 5)
@@ -32,17 +31,16 @@ def test_curved():
     x, y, z = state.SpatialCoordinate()
     r_i = 30e-9
     r_a = 50e-9
-    state.material = {"sigma": 1.}
-    state.material["sigma"] = (x**2+y**2 > r_i**2) & (x**2+y**2 < r_a**2)
+    state.material["sigma"] = 1. * ((x**2+y**2 > r_i**2) & (x**2+y**2 < r_a**2))
     write_vti(state.material, "data/material.vti")
-    
-    dirichlet_bc_u0 = state._zeros([n+2 for n in state.mesh.n])
-    dirichlet_bc_nodes = state._zeros([n+2 for n in state.mesh.n], dtype=bool)
+
+    dirichlet_bc_nodes = state.Constant(False, dtype=bool)
     dirichlet_bc_nodes[:,0,:] = True
 
-    dirichlet_bc_u0[n[0]//2:,0,:] = 0.
-    dirichlet_bc_u0[:n[0]//2,0,:] = 1.
+    state.u = state.Constant(0.)
+    state.u[:n[0]//2,0,:] = 1.
 
-    ohm = OhmSolver(dirichlet_bc_nodes, dirichlet_bc_u0)
+    ohm = OhmSolver(dirichlet_bc_nodes)
     u = ohm.u(state)
-    write_vti(u, "data/u.vti")
+    j = ohm.j(state)
+    write_vti({"u":u, "j":j}, "data/u.vti")
