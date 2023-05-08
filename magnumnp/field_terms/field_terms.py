03877f8fd211a0927d1cf0e79bae2a277bdf1490
Florian Bruckner <florian.bruckner@univie.ac.at>
2023-05-08 12:57:35 +0200

    add domain parameter to field_term.E

--- magnumnp/field_terms/field_terms.py
+++ magnumnp/field_terms/field_terms.py
@@ -34,5 +34,6 @@ class FieldTerm(object):
             setattr(self, key, value)
 
 class LinearFieldTerm(FieldTerm):
-    def E(self, state):
-        return -0.5 * constants.mu_0 * torch.sum(state.material["Ms"] * state.m * self.h(state) * state.cell_volumes)
+    def E(self, state, domain = Ellipsis):
+        E = -0.5 * constants.mu_0 * state.material["Ms"] * state.m * self.h(state) * state.cell_volumes
+        return E[domain].sum()
--- tests/unit/fieldterm_test.py
+++ tests/unit/fieldterm_test.py
@@ -227,3 +227,27 @@ def test_energy_nonequidistant():
     E2 = aniso.E(state)
 
     torch.testing.assert_close(E1, 5*E2, atol=0, rtol=1e-15)
+
+def test_energy_domain():
+    n  = (10, 5, 4)
+    dx2 = torch.ones(n[2]) * 5e-9
+    dx2[2:] = 1e-9
+    dx = (1e-9, 2e-9, dx2)
+
+    mesh = Mesh(n, dx)
+    state = State(mesh)
+
+    state.material = {"Ms":      state.Tensor([1.]),
+                      "Ku":      state.Tensor([1.]),
+                      "Ku_axis": state.Tensor([0,1,0])}
+
+    aniso = UniaxialAnisotropyField()
+
+    state.m = state.Constant([1,1,0])
+    domain1 = state.Constant(True, dtype=torch.bool)
+    domain1[:,:,2:] = False
+
+    E1 = aniso.E(state, domain1)
+    E2 = aniso.E(state, ~domain1)
+
+    torch.testing.assert_close(E1, 5*E2, atol=0, rtol=1e-15)
