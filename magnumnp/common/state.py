fixed energy calculation for nonequidistant grid

magnumnp/common/state.py
@@ -39,7 +39,6 @@ class State(object):
             self._device = device
 
         #TODO: add scale parameter to fix paraview issue, and use characteristic length scales
-
         self._dtype = dtype or torch.get_default_dtype()
         self.mesh = mesh
 
@@ -48,7 +47,7 @@ class State(object):
 
         # compute cell_volumes (use expand for equidistant dimentions)
         dx, dy, dz = torch.meshgrid([self._tensor(dx) for dx in mesh.dx], indexing = "ij")
-        self._cell_volumes = (dx*dy*dz).expand(mesh.n)
+        self._cell_volumes = (dx*dy*dz).expand(mesh.n).unsqueeze(-1)
 
         self._material = Material(self)
         self.t = t0

magnumnp/field_terms/field_terms.py
@@ -35,4 +35,4 @@ class FieldTerm(object):
 
 class LinearFieldTerm(FieldTerm):
     def E(self, state):
-        return -0.5 * constants.mu_0 * state.mesh.cell_volume * torch.sum(state.material["Ms"] * state.m * self.h(state))
+        return -0.5 * constants.mu_0 * torch.sum(state.material["Ms"] * state.m * self.h(state) * state.cell_volumes)

tests/unit/inverse_test.py
@@ -42,6 +42,7 @@ def test_hext(simple_state):
                                        BulkDMIField(),
                                        D2dDMIField()])
 def test_linear_fieldterms(simple_state, fieldterm):
+    cell_volume = simple_state.mesh.dx[0] * simple_state.mesh.dx[1] * simple_state.mesh.dx[2]
     x,y,z = simple_state.SpatialCoordinate()
     m0 = torch.stack([x,2*x,y], dim=-1)
     m0.requires_grad = True
@@ -54,6 +55,6 @@ def test_linear_fieldterms(simple_state, fieldterm):
 
     L = forward(m0)
     L.backward()
-    h1 = -m0.grad / (constants.mu_0 * simple_state.material["Ms"] * simple_state.mesh.cell_volume)
+    h1 = -m0.grad / (constants.mu_0 * simple_state.material["Ms"] * cell_volume)
 
     torch.testing.assert_close(h0/h0.abs().max(), h1/h0.abs().max(), atol=1e-4, rtol=1e-4)