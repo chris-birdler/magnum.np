89c26e2dd32892d2e64680bbd8eb73cb48a5c45d
Florian Bruckner <florian.bruckner@univie.ac.at>
2023-05-19 15:55:44 +0200

    added sabris ohm solver test

--- tests/unit/ohm_solver_test.py
+++ tests/unit/ohm_solver_test.py
@@ -66,3 +66,28 @@ def test_curved_2D():
     u = ohm.u(state)
     j = ohm.j(state)
     write_vti({"u":u, "j":j}, "data/u.vti")
+
+def test_sabri():
+    n  = (1000, 1000, 1)
+    dx = (5e-9, 5e-9, 1e-9)
+    mesh = Mesh(n, dx, origin = (-n[0]*dx[0]/2.,-n[1]*dx[1]/2.,-n[2]*dx[2]/2.))
+    state = State(mesh)
+    x, y, z = state.SpatialCoordinate()
+
+    domain = (x**2 < 1.0e-6**2) | (y**2 < 1.0e-6**2)
+    state.material["sigma"] = 0.
+    state.material["sigma"][domain] = 5.3e5
+
+    write_vti(state.material, "data/material.vti")
+
+    dirichlet_bc_nodes = state.Constant(False, dtype=bool)
+    dirichlet_bc_nodes[0,:,:] = True
+    dirichlet_bc_nodes[:,0,:] = True
+
+    state.u = state.Constant(0.)
+    state.u[0,:,:] = 1.
+
+    ohm = OhmSolver(dirichlet_bc_nodes)
+    u = ohm.u(state, max_iter = 1000)
+    j = ohm.j(state, max_iter = 1000)
+    write_vti({"u":u, "j":j}, "data/u.vti")
