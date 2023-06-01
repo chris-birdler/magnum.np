74b92d9263f65de05b475924bc2efc2209456d47
Florian Bruckner <florian.bruckner@univie.ac.at>
2023-06-01 16:12:32 +0200

    added calculation of 3D eigenvectors

--- demos/eigensolver/run.py
+++ demos/eigensolver/run.py
@@ -1,45 +1,33 @@
 from magnumnp import *
 from torch import sqrt
 
-
-
 n = (20, 20, 1)
 dx = (10e-9, 10e-9, 1e-9)
 origin = (-1./2.*n[0]*dx[0], -1./2.*n[1]*dx[1], -1./2.*n[2]*dx[2])
+
 mesh = Mesh(n, dx, origin)
 state = State(mesh)
+
 x,y,z = state.SpatialCoordinate()
 r = x**2 + y**2
+
 state.m = state.Tensor(torch.stack([y/r, -x/r, 1e-9/r], dim = -1))
 state.m.normalize()
-write_vti(state.m, "m0.vti")
 
-state.material = {'Ms': 800e3, 'A': 13e-12, 'Ku': -5e3, 'Ku_axis': (0,0,1)}
+state.material = {"Ms": 800e3,
+                  "A": 13e-12,
+                  "Ku": -5e3,
+                  "Ku_axis": (0,0,1)}
 
-class EigenSolverTest(object):
-    def __init__(self, state, linear_terms, constant_terms):
-        #self._linear_terms = linear_terms
-        #self._constant_terms = constant_terms
-        #h = sum([term.h(state) for term in self._terms])
- 
-        self._state = state
- 
-        ez = state.Constant([1e-15,0.,1.])
-        self._e1 = torch.linalg.cross(ez, self._state.m)
-        self._e1 = self._e1 / torch.linalg.norm(self._e1, axis=3).unsqueeze(dim = -1)
-        self._e0 = -torch.linalg.cross(self._e1, self._state.m)
-        self._e0 = self._e0 / torch.linalg.norm(self._e0, axis=3).unsqueeze(dim = -1)
- 
-        print(self._e0.shape, self._e1.shape)
-        write_vti(self._e0, "e0.vti")
-
-eig = EigenSolverTest(state, [], [])
 exchange = ExchangeField()
 demag = DemagField()
 external = ExternalField([10e3, 0, 0])
 eigs = EigenSolver(state, [demag, exchange], [external])
 res = eigs.solve(k = 5)
+#omega, evecs2D, state, m0, D0 = eigs.solve(k = 5)
 
-
-print(res[0])
-print(res[1].shape)
+evecs = res.evecs()
+print("f[GHz]:",  res.omega/2./torch.pi*1e-9)
+res.save_evecs3D("data/evecs.vti")
+#print(res[0])
+#print(res[1].shape)
--- magnumnp/solvers/eigensolver.py
+++ magnumnp/solvers/eigensolver.py
@@ -16,13 +16,13 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from magnumnp.common import constants
+from magnumnp.common import logging, constants, write_vti
 import torch
 import numpy as np
 from scipy.sparse.linalg import LinearOperator, aslinearoperator, eigs
 from scipy.linalg import eig
 
-__all__ = ["EigenSolver"]
+__all__ = ["EigenSolver", "EigenResult"]
 
 class EigenSolver(object):
     def __init__(self, state, linear_terms, constant_terms):
@@ -46,7 +46,7 @@ class EigenSolver(object):
     def _D0(self, vv):
         self._it += 1
         if self._it % 500 == 0:
-            print("D0(2D):", self._it)
+            logging.info_blue("[Eigensolver] it= %d" % self._it)
 
         vv = self._state.Tensor(torch.from_numpy(vv))
         vv = vv.reshape(self._m0.shape[:3] + (2,))
@@ -61,85 +61,67 @@ class EigenSolver(object):
         h -= self._h0*vvv
 
         # apply B0
-        rrr = -constants.gamma * torch.linalg.cross(self._m0.to(dtype = torch.complex128), h)
+        rrr = -constants.gamma * torch.linalg.cross(self._m0.to(dtype = h.dtype), h)
 
-        # apply R^T
-        rr = self._state.zeros(vv.shape[:3] + (2,), dtype=torch.complex128)
-        rr[:,:,:,0] = (rrr*self._e0).sum(dim=3)
-        rr[:,:,:,1] = (rrr*self._e1).sum(dim=3)
+        # apply R^T (reuse vv tensor)
+        vv[:,:,:,0] = (rrr*self._e0).sum(dim=-1)
+        vv[:,:,:,1] = (rrr*self._e1).sum(dim=-1)
 
-        return rr.reshape(-1).detach().cpu().numpy()
+        return vv.reshape(-1).detach().cpu().numpy()
 
     def solve(self, k=10, tol=0, method="eigs"):
         N = np.prod(self._m0.shape[:3])
         D0 = LinearOperator((2*N,2*N), self._D0, dtype=np.complex128)
 
-        if method == "eigs":
-            evals, evecs2D = eigs(D0, k = 2*k, which = 'SM', tol = tol)
-            #evals, evecs2D = eigs(D0, k = 2*k, sigma = 0, which = 'LM', tol = tol)
+        evals, evecs2D = eigs(D0, k = 2*k, which = 'SM', tol = tol)
+        #evals, evecs2D = eigs(D0, k = 2*k, sigma = 0, which = 'LM', tol = tol)
 
-            evalvecs_sorted = sorted(zip(evals,evecs2D.T), key=lambda x: np.abs(x[0].imag))
-            evals = np.array([x[0] for x in evalvecs_sorted if x[0].imag > 1000.])
-            evecs2D = np.array([x[1] for x in evalvecs_sorted if x[0].imag > 1000.]).transpose()
-        elif method == "eig":
-            D0_op = matrix_from_operator(D0)
-            evals, evecs2D = eig(D0_op)
+#        evalvecs_sorted = sorted(zip(evals,evecs2D.T), key=lambda x: np.abs(x[0].imag))
+#        evals = np.array([x[0] for x in evalvecs_sorted if x[0].imag > 1000.])
+#        evecs2D = np.array([x[1] for x in evalvecs_sorted if x[0].imag > 1000.]).transpose()
 
-            evalvecs_sorted = sorted(zip(evals,evecs2D.T), key=lambda x: np.abs(x[0].imag))
-            evals = np.array([x[0] for x in evalvecs_sorted if x[0].imag > 1000.])[:k]
-            evecs2D = np.array([x[1] for x in evalvecs_sorted if x[0].imag > 1000.]).transpose()[:,:k]
-
-        return evals, evecs2D
-
-    def save_evecs3D(self, evecs2D, filename):
-        vv = evecs2D.reshape(self.m0.shape[:3] + (2,-1))
-        for i in list(range(vv.shape[-1])):
-            vvv = vv[:,:,:,(0,),i]*self._e0 + vv[:,:,:,(1,),i]*self._e1
-            write_vtr.write_vtr(np.abs(vvv), "%s_%02d" % (filename, i))
+        omega = evals.imag
+        evecs2D = self._state.Tensor(torch.from_numpy(evecs2D)).reshape(self._m0.shape[:3] + (2,-1))
+        return EigenResult(omega, evecs2D, self._state, m0 = self._m0, e0 = self._e0, e1 = self._e1, D0 = D0)
 
 
-##############################
-### some utility functions ###
-##############################
-def matrix_from_operator(op, N=None): # NOTE: this may change m0
-    if N is None:
-        N = aslinearoperator(op).shape[1]
+class EigenResult(object):
+    def __init__(self, omega, evecs2D, state, **kwargs):
+        self.omega = omega
+        self._evecs2D = evecs2D
+        self._state = state
+        self.__dict__.update(kwargs)
 
-    mat = np.zeros((op.shape[0],N), dtype=np.complex128)
-    for i in range(N):
-        m = np.zeros(op.shape[1])
-        m[i] = 1.0
-        mat[:,i] = aslinearoperator(op)(m)
-    return mat[:N,:N]
+    def store(self, filename):
+        torch.save({"m0":self.m0, "omega":self.omega, "evecs2D":self._evecs2D}, filename)
+        logging.info_green("[Eigensolver] Stored %d eigenvalues to '%s'it= %d" % (len(self.omega), filename))
 
-def plot_operator(op, filename, N=None, log=False):
-    from matplotlib import pyplot as plt
-    from matplotlib import cm as cm
+#    @staticmethod
+#    def load(state, filename):
+#        stored = np.load(filename)
+#        omega, evecs2D = stored['omega'], stored['evecs2D']
+#
+#        m0 = fd.Function(state.m.function_space())
+#        m0.vector()[:] = stored['m0']
+#        state.m = m0
+#
+#        solver = EigenSolverBase(state)
+#        logging.info_green("%s: Loaded %d eigenvalues from '%s'" % (__class__.__name__, len(omega), filename))
+#        return EigenResult(omega, evecs2D, state, m0 = m0, R = PETSc2CSR(solver.R), A0 = PETSc2Scipy(solver.A0), B0 = 1j*PETSc2Scipy(solver.B0))
 
-    if N == None:
-        N = op.shape[0]
-    X = matrix_from_operator(op, N=N)
-    #X = X.imag.copy()
-    X = np.absolute(X)
+    def evecs(self, N = slice(None)):
+        vvv = self._evecs2D[:,:,:,(0,),:]*self.e0[:,:,:,:,None] + self._evecs2D[:,:,:,(1,),:]*self.e1[:,:,:,:,None]
+        return vvv[...,N]
 
-    if log is True:
-        X = np.log(np.abs(X))
-
-    fig, ax = plt.subplots()
-    img = ax.imshow(X, cmap=cm.jet, interpolation='nearest')#, vmin=-50, vmax=30)
-    numrows, numcols = X.shape
-
-    def format_coord(x, y):
-        col = int(x + 0.5)
-        row = int(y + 0.5)
-        if col >= 0 and col < numcols and row >= 0 and row < numrows:
-            z = X[row, col]
-            return 'x=%1.4g, y=%1.4g, z=%1.4g' % (x, y, z)
+    def save_evecs3D(self, filename, which = "abs", N = slice(None)):
+        if which == "abs":
+            op = torch.abs
+        elif which == "real":
+            op = torch.real
+        elif which == "imag":
+            op = torch.imag
         else:
-            return 'x=%1.4g, y=%1.4g' % (x, y)
+            op = which
 
-    ax.format_coord = format_coord
-    fig.colorbar(img)
-    plt.savefig(filename)
-    return X
-
+        evecs_list = [op(v.squeeze(-1)) for v in self.evecs(N = N).split(1,dim=-1)]
+        write_vti(evecs_list, filename, self._state) # TODO: write pvd instead of vti
