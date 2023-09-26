a809e8976af54015d41fb6fd9d75650674b6c9bd
Florian Bruckner <florian.bruckner@univie.ac.at>
2023-09-26 11:57:03 +0000

    fixed Langevin demo

--- demos/thermal/langevin.py
+++ demos/thermal/langevin.py
@@ -1,41 +1,46 @@
 from magnumnp import *
 import numpy as np
 
-set_log_level(10)
+"""
+Stochastic Time-Integration Demo
+
+taken from Leliaert et al., "Adaptively time stepping the stochastic Landau-Lifshitz-Gilbert equation at nonzero temperature:
+                             Implementation and validation in MuMax 3", AIP advances 7, 125010 (2017)
+
+For comparison with the paper Leliaert et al., AIP advances 7, 125010 (2017) Figure 5 we need to simulate a 2**18 (64**3) of uncoupled cells.
+Since the cell are uncoupled the volume in our calculations has to be the volume of one cell and not the whole simulated material.
+"""
+
+set_log_level(0)
 Timer.enable()
 
-n  = (64, 64, 64)
-dx = (1e-9, 1e-9, 1e-9)
+n  = (64, 64, 64) # 2**18 of uncoupled cells.
+dx = (10e-9, 10e-9, 10e-9)
 mesh = Mesh(n, dx)
 
-eta = 30
-Js = 1.
+Ms = 1e6
+t_final = 2e-8
 
 state = State(mesh)
 state.material = {
-    "Ms": Js/constants.mu_0,
-    "alpha": 1.0
+    "Ms": Ms, # Js ~ 1.26 T for Ms = 1e6 A/m
+    "alpha": 0.1
     }
-state.m = state.Constant([0,0,1])
+state.m = state.Constant([0,0,0])
+add_noise(state.m)
 state.m.normalize()
-state.T = Js * state.cell_volumes.max() * np.prod(mesh.n) / (constants.kb * eta)
-print("T:", state.T.numpy())
-#print("cell_volume:", state.cell_volumes)
 
-aniso = UniaxialAnisotropyField()
 external = ExternalField([0,0,0])
+llg = LLGSolver([external], solver = RKF45)
 
-llg = LLGSolver([external], solver=Heun)
-logger = Logger("data", ['t', external.h, 'm'])
-
-# relax
-for i in range(10000):
-    llg.step(state, 1e-13)
-
-for h in np.linspace(0,0.1, num=11):
-    external.h = [h / constants.mu_0, 0, 0]
-    for i in range(10000):
-        llg.step(state, 1e-13)
-    logger << state
-        
+# perform stochastic integration
+for xi in [30, 91, 242, 725]:
+    logger = ScalarLogger("data/log_xi%d.dat" % xi, ['t', external.h, 'm'])
+    state.T = constants.mu_0 * Ms * state.cell_volumes.max() * 1./constants.mu_0 / (constants.kb * xi)
+    print(f"Running for xi = {xi} (T = {state.T})")
+    for h in np.linspace(0, 0.1, num=11):
+        external.h = [h / constants.mu_0, 0, 0]
+        llg.step(state, dt = t_final)
+        logger << state
 Timer.print_report()
+
