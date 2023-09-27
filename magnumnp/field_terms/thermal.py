82aa1c88c0302c86902e89004dc70b3752d6a21f
Florian Bruckner <florian.bruckner@univie.ac.at>
2023-09-27 07:46:07 +0000

    use compile for thermal field

--- magnumnp/field_terms/thermal.py
+++ magnumnp/field_terms/thermal.py
@@ -36,4 +36,10 @@ class ThermalField(FieldTerm):
             self._sigma = state._normal(0., 1., size = state.m.shape)
             self._step = state._step
             
-        return self._sigma * torch.sqrt(2. * state.material["alpha"] * constants.kb * state.T / (constants.mu_0 * state.material["Ms"] * constants.gamma * state.cell_volumes * state._dt))
+        alpha = state.material["alpha"].torch_tensor
+        Ms = state.material["Ms"].torch_tensor
+        return self._h(self._sigma, alpha, Ms, state)
+
+    @torch.compile
+    def _h(self, sigma, alpha, Ms, state):
+        return sigma * torch.sqrt(2. * alpha * constants.kb * state.T / (constants.mu_0 * Ms * constants.gamma * state.cell_volumes * state._dt))
