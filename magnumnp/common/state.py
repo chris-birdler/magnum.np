fixed hext gradient test

magnumnp/common/state.py
@@ -66,7 +66,8 @@ class State(object):
             c = c.reshape(1)
         x = torch.zeros(self.mesh.n + c.shape, dtype = dtype)
         x[...] = c
-        x.requires_grad = requires_grad
+        if requires_grad == True:
+            x.requires_grad = requires_grad
         return x
 
     def SpatialCoordinate(self):

magnumnp/field_terms/external.py
@@ -43,8 +43,9 @@ class ExternalField(object):
         h = torch.stack([x,y,z], dim=-1)
         external = ExternalField(h)
     """
-    def __init__(self, h):
-        self.__setattr__("h", h)
+    def __init__(self, h = None):
+        if h != None:
+            self.__setattr__("h", h)
 
     @timedmethod
     def h(self, state):