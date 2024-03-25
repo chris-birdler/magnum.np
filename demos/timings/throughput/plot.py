96541b6639fa0966d1e7fb5d578900ab9f96cd6a
Florian Bruckner <florian.bruckner@univie.ac.at>
2024-03-25 14:32:10 +0100

    minor changes

--- demos/timings/throughput/plot.py
+++ demos/timings/throughput/plot.py
@@ -11,7 +11,7 @@ fig, ax = plt.subplots(figsize=(15, 5))
 cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 ax.loglog(data[:,0], data[:,3]/1e6, color = cycle[0], label = "magnum.np")
-ax.loglog(data[:,0], ref[:,3]/1e6, '-', color = cycle[0], linewidth = 6, alpha = 0.4, label = "magnum.np(ref)")
+ax.loglog(ref[:,0], ref[:,3]/1e6, '-', color = cycle[0], linewidth = 6, alpha = 0.4, label = "magnum.np(ref)")
 ax.loglog(mumax[:,0], mumax[:,1]/1e6, 'k--', label = "MuMax3")
 
 ax.set_xlabel("Number of Cells N")
