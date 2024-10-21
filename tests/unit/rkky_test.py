db61865db5b979a662286c6b8eb9cf93d1b48a16
Florian Bruckner <florian.bruckner@univie.ac.at>
2024-10-21 12:32:20 +0200

    added test_rkky_biquadratic_nonequi()

--- tests/unit/rkky_test.py
+++ tests/unit/rkky_test.py
@@ -122,3 +122,44 @@ def test_rkky_nonequi():
         torch.testing.assert_close(h1, h1_analytic, atol=1e-10, rtol=1e-10)
         torch.testing.assert_close(h2, h2_analytic, atol=1e-10, rtol=1e-10)
         assert E == pytest.approx(E_analytic, rel=1e-10, abs=1e-25)
+
+def test_rkky_biquadratic_nonequi():
+    J_rkky = 0.001
+    n  = (10, 10, 2)
+    dx = (1e-9, 1e-9, [5e-9, 5e-9])
+    L  = (n[0]*dx[0], n[1]*dx[1], n[2]*dx[2])
+    mesh = Mesh(n, dx)
+
+    state = State(mesh)
+    state.material = {"Ms":1./constants.mu_0, "A":1e-11}
+    state.m = state.Constant([1,0,0])
+
+    domain1 = state.Constant(False, dtype=torch.bool)
+    domain1[:,:,0] = True
+
+    domain2 = state.Constant(False, dtype=torch.bool)
+    domain2[:,:,1] = True
+
+    exchange1 = ExchangeField(domain1)
+    exchange2 = ExchangeField(domain2)
+    rkky = BiquadraticRKKYField(J_rkky, "z", 0, 1)
+
+    for phi in torch.linspace(0,2*pi,36):
+        state.m[domain2] = torch.tensor([cos(phi), sin(phi), 0])
+        h = rkky.h(state)
+
+        h1 = h[0,0,0,:]
+        h2 = h[0,0,1,:]
+
+        m1 = state.m[0,0,0,:]
+        m2 = state.m[0,0,1,:]
+
+        h1_analytic = 2.*J_rkky/dx[2][0]*(m1*m2).sum()*m2
+        h2_analytic = 2.*J_rkky/dx[2][1]*(m1*m2).sum()*m1
+
+        E = rkky.E(state).detach().cpu().numpy()
+        E_analytic = -J_rkky*L[0]*L[1]*cos(phi)**2
+
+        torch.testing.assert_close(h1, h1_analytic, atol=1e-10, rtol=1e-10)
+        torch.testing.assert_close(h2, h2_analytic, atol=1e-10, rtol=1e-10)
+        assert E == pytest.approx(E_analytic, rel=1e-10, abs=1e-25)
