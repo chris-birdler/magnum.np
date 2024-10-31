22bdaba0bdc74d74f6fc8a3a913b89f57fc48134
Florian Bruckner <florian.bruckner@univie.ac.at>
2024-10-31 16:31:26 +0100

    rearranged notebook CI job

--- demos/colab_header.py
+++ demos/colab_header.py
@@ -1,5 +1,6 @@
 # %% [markdown]
-# This jupyter-notebook has be created with 'jupytext'
+# This jupyter-notebook has be created with 'jupytext'.
+# Use this [Link](https://colab.research.google.com/github/magnumnp/magnumnp_demos/blob/main/DEMO.ipynb) to directly open the Notebook in Google Colab.
 
 # %% [markdown]
 # ## Install magnum.np and fetch reference data (required for Colab)
@@ -9,4 +10,4 @@
 from os import path
 if not path.isdir("ref"):
     !mkdir ref
-    !wget -P ref https://gitlab.com/magnum.np/magnum.np/raw/main/demos/sp4/ref/m.dat &> /dev/null
+    !wget -P ref https://gitlab.com/magnum.np/magnum.np/raw/main/demos/DEMO/ref/m.dat &> /dev/null
