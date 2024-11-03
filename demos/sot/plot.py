3a3c6fa23fe201af6bbb81967dc1bf0bf313f830
Florian Bruckner <florian.bruckner@univie.ac.at>
2024-11-03 01:09:33 +0100

    adapted softmagnetic_composite and SOT demo

--- demos/sot/plot.py
+++ demos/sot/plot.py
@@ -1,3 +1,7 @@
+# %% [markdown]
+# ## Plot Results
+
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -5,15 +9,13 @@ data = np.loadtxt("data/log.dat")
 ref_np = np.loadtxt("ref/ref_magnumnp.dat")
 ref_af = np.loadtxt("ref/ref_magnumaf.dat")
 
-fig, ax = plt.subplots(figsize=(10,5))
+fig, ax = plt.subplots(figsize=(15,5))
 cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 ax.plot(data[:,1], data[:,2], '-', color = cycle[2], label = "magnum.np")
 ax.plot(ref_np[:,1], ref_np[:,2], '-', color = cycle[2], linewidth = 6, alpha = 0.4, label = "reference magnum.np")
 ax.plot(ref_af[:,1], ref_af[:,2], '--', color = cycle[2], linewidth = 1, alpha = 0.8, label = "reference magnum.af")
 
-#ax.set_xlim([4.82,5.0])
-#ax.set_ylim([0.99994,1.0])
 ax.set_title("Spin Orbit Torque")
 ax.set_xlabel("Magnetization $m_x$")
 ax.set_ylabel("Magnetization $m_y$")
--- demos/sot/sot_test.py
+++ demos/sot/sot_test.py
@@ -1,13 +1,11 @@
 import pytest
 import torch
 from magnumnp import *
-from sot_run import run_sot
+import run
 import numpy as np
 import pathlib
 
 def test_sot():    
-    run_sot()
-    
     this_dir = pathlib.Path(__file__).resolve().parent
     data_path = this_dir / "data" / "log.dat"
     ref_path = this_dir / "ref" / "ref_test.dat"
@@ -22,4 +20,4 @@ def test_sot():
     ref_y = torch.from_numpy(ref[:,2])
     
     torch.testing.assert_close(data_x, ref_x, atol=1e-3, rtol=1e-3)
-    torch.testing.assert_close(data_y, ref_y, atol=1e-3, rtol=1e-3) 
+    torch.testing.assert_close(data_y, ref_y, atol=1e-3, rtol=1e-3)
