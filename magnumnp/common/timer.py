37487e6f78ec2b9edeb846ea7c49a64f24c0168e
Florian Bruckner <florian.bruckner@univie.ac.at>
2025-04-02 12:23:54 +0200

    renamed default logging method to default

--- magnumnp/common/timer.py
+++ magnumnp/common/timer.py
@@ -166,12 +166,12 @@ class Timer(object):
         table = "\n".join(lines)
 
         width = len(table.split("\n")[1])
-        logging.print("=" * width)
-        logging.print("TIMER REPORT")
-        logging.print("=" * width)
+        logging.default("=" * width)
+        logging.default("TIMER REPORT")
+        logging.default("=" * width)
         for line in table.split('\n'):
-            logging.print(line)
-        logging.print("=" * width)
+            logging.default(line)
+        logging.default("=" * width)
         missing = missing_time / total_time
         if missing > 0.2:
             logging.warning("Too much time missing (%.0f%%). Add some Timers for more complete timing!" % (missing * 100.))
